4c149daf51c3157931838cab71c0e0490c659443
VETRIVEL <vetrivel.galaxy@gmail.com>
2022-01-24 18:52:40 +0300

    Changes : Resume template bugs fixed

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 const Template = ({ data: { id, name, imageSrc } }) => {
   return (
     <div className=" flex justify-center items-center cursor-pointer  ">
-      <div className="group h-510 relative overflow-hidden">
-        <img src={imageSrc} alt="" className=" w-350 h-ful" />
-        <div className="absolute top-0 left-0 right-0 bottom-0 group-hover:bg-blackOverlay opacity-70"></div>
+      <div className="group h-510 relative overflow-hidden rounded-lg">
+        <img
+          src={imageSrc}
+          alt=""
+          className=" w-350 h-510 group-hover:opacity-70 rounded-lg"
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 group-hover:bg-blackOverlay duration-150 transition-all ease-in-out"></div>
 
         <div
-          className="absolute  top-5 left-2 text-white -translate-y-10 
+          className="absolute  top-5 left-2 text-white -translate-y-20 font-bold text-2xl
         group-hover:translate-y-0 group-hover:delay-150 duration-100 ease-in-out transition-all"
         >
           {name}
